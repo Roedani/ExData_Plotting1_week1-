a888186d1200001a845122ceec36c2a0797d3620
Roedani <74432483+Roedani@users.noreply.github.com>
2021-03-18 16:34:15 -0400

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,7 @@
+
+## preparing data for the second plot
+date_time <- strptime(paste(new_data$Date, new_data$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+new_data$Global_active_power <- as.numeric(new_data$Global_active_power)
+png("plot2.png", width=480, height=480)
+plot(date_time, new_data$Global_active_power, "l", xlab = "", ylab="Global Active Power (kilowatts)")
+dev.off()
