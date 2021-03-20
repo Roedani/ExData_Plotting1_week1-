87648752af523dcf8e854dc160de6c322bac1ade
Roedani <74432483+Roedani@users.noreply.github.com>
2021-03-19 22:33:04 -0400

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,4 +1,11 @@
 
+### uploading the data
+data_file <- ("household_power_consumption.txt")
+data <- read.table(data_file, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+
+### substting the dates to be used for plot
+new_data <- subset(data[data$Date %in% c("1/2/2007","2/2/2007"), ])
+
 ## preparing data for the second plot
 date_time <- strptime(paste(new_data$Date, new_data$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 new_data$Global_active_power <- as.numeric(new_data$Global_active_power)
