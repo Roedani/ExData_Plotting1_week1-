744850ec8abaece5ec67dd00db52ed8b63edda2b
Roedani <74432483+Roedani@users.noreply.github.com>
2021-03-19 22:51:27 -0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -7,7 +7,7 @@ data <- read.table(data_file, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=
 ### substting the dates to be used for plot
 new_data <- subset(data[data$Date %in% c("1/2/2007","2/2/2007"), ])
 
-### the script for make the plot
+### the script to make plot1
 new_data$Global_active_power <- as.numeric(new_data$Global_active_power)
 png("plot1.png", width=480, height=480)
 hist(new_data$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
